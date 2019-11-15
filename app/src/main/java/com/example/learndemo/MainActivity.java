169402d22cd3f9737241fbba35466ff94c8b4382
xiaofeng.liu <liuxiaofeng54222>
2019-11-15 20:29:16 +0800

    view项目创建

--- app/src/main/java/com/example/learndemo/MainActivity.java
+++ app/src/main/java/com/example/learndemo/MainActivity.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewStub viewStub;
+    private TextView tvViewStub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewStub = findViewById(R.id.viewstub);
+        viewStub.inflate();
+        tvViewStub = findViewById(R.id.tv_viewstub);
+        tvViewStub.setText("asd");
     }
 
     /**
